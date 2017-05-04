02add76865bb0841a5cadebf24038482910463fb
Prasad <prasad7779@gmail.com>
2017-05-04 13:30:23 +0200

    Modified to collect Survey Sensor data

--- app/src/main/java/ch/ethz/coss/nervousnet/assetdemousingaar/MainActivity.java
+++ app/src/main/java/ch/ethz/coss/nervousnet/assetdemousingaar/MainActivity.java
@@ -1,21 +1,46 @@
 package ch.ethz.coss.nervousnet.assetdemousingaar;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.greenrobot.eventbus.EventBus;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import ch.ethz.coss.nervousnet.lib.InfoReading;
+import ch.ethz.coss.nervousnet.lib.LibConstants;
+import ch.ethz.coss.nervousnet.lib.NervousnetServiceConnectionListener;
+import ch.ethz.coss.nervousnet.lib.NervousnetServiceController;
+import ch.ethz.coss.nervousnet.lib.RemoteCallback;
+import ch.ethz.coss.nervousnet.lib.SensorReading;
 import ch.ethz.coss.nervousnet.vm.NNLog;
 import ch.ethz.coss.nervousnet.vm.NervousnetVM;
 import ch.ethz.coss.nervousnet.vm.NervousnetVMConstants;
 import ch.ethz.coss.nervousnet.vm.events.NNEvent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RemoteCallback {
 
 
     public NervousnetVM nn_VM;
-
+    private RadioGroup radioSurvey;
+    private RadioButton radioButton;
+    private Button btnSubmit, btnFetch;
+    private TextView ansTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +49,18 @@ public class MainActivity extends AppCompatActivity {
 
         nn_VM = new NervousnetVM(getApplicationContext());
 
-        final Switch mainSwitch, accelerometerSwitch;
-        final Switch batterySwitch , gyroSwitch, locationSwitch;
+        final Switch mainSwitch;
+
+
+
 
         mainSwitch = (Switch) findViewById(R.id.switch1);
-        if(nn_VM.getState() == NervousnetVMConstants.STATE_RUNNING)
+        if(nn_VM.getNervousnetState() == NervousnetVMConstants.STATE_RUNNING)
             mainSwitch.setChecked(true);
         else
             mainSwitch.setChecked(false);
 
-        accelerometerSwitch = (Switch) findViewById(R.id.switch2);
-        batterySwitch = (Switch) findViewById(R.id.batt);
-        gyroSwitch = (Switch) findViewById(R.id.gyro);
-        locationSwitch = (Switch) findViewById(R.id.loc);
 
-        if(nn_VM.getState() == NervousnetVMConstants.STATE_RUNNING)
-            mainSwitch.setChecked(true);
-        else
-            mainSwitch.setChecked(false);
 
 
         mainSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -50,65 +69,138 @@ public class MainActivity extends AppCompatActivity {
                 if(mainSwitch.isChecked()) {
 
                     EventBus.getDefault().post(new NNEvent(NervousnetVMConstants.EVENT_START_NERVOUSNET_REQUEST));
-                    accelerometerSwitch.setEnabled(true);
-                    batterySwitch.setEnabled(true);
-                    gyroSwitch.setEnabled(true);
-                    locationSwitch.setEnabled(true);
                 }
                 else {
                     EventBus.getDefault().post(new NNEvent(NervousnetVMConstants.EVENT_PAUSE_NERVOUSNET_REQUEST));
-                    accelerometerSwitch.setChecked(false);
-                    batterySwitch.setChecked(false);
-                    gyroSwitch.setChecked(false);
-                    locationSwitch.setChecked(false);
 
-                    accelerometerSwitch.setEnabled(false);
-                    batterySwitch.setEnabled(false);
-                    gyroSwitch.setEnabled(false);
-                    locationSwitch.setEnabled(false);
                 }
             }
         });
 
-        accelerometerSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                byte state = accelerometerSwitch.isChecked() ? NervousnetVMConstants.SENSOR_STATE_AVAILABLE_DELAY_LOW : NervousnetVMConstants.SENSOR_STATE_AVAILABLE_BUT_OFF;
-                NNLog.d("MainActivity", "Accelerometer state = "+state);
 
-                EventBus.getDefault().post(new NNEvent((long) 0, state, NervousnetVMConstants.EVENT_CHANGE_SENSOR_STATE_REQUEST));
+
+
+        radioSurvey = (RadioGroup) findViewById(R.id.radioSurvey);
+        btnSubmit = (Button) findViewById(R.id.button);
+        btnFetch = (Button) findViewById(R.id.Fetch);
+
+        ansTV = (TextView) findViewById(R.id.textView3);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if(nn_VM.getNervousnetState() == NervousnetVMConstants.STATE_RUNNING) {
+                    // get selected radio button from radioGroup
+                    int selectedId = radioSurvey.getCheckedRadioButtonId();
+
+                    // find the radiobutton by returned id
+                    radioButton = (RadioButton) findViewById(selectedId);
+
+
+//                    try {
+//                        nervousnetServiceController.getLatestReading(10002);
+//                    } catch (RemoteException e) {
+//                        e.printStackTrace();
+//                    }
+
+                    String [] params = new String[] {
+                            "surveyID",
+                            "qID",
+                            "question",
+                            "type",
+                            "option1",
+                            "option2",
+                            "option3",
+                            "option4",
+                            "chosenAnswer"
+                    };
+                    ArrayList<String> paramNames = new ArrayList<String>(Arrays.asList(params));
+                    SensorReading surveyReading = new SensorReading(10002, "SurveySensor", paramNames);
+
+                    String [] values = new String[] {
+                            "survey-id-1",
+                            "question-id-1",
+                            "How useful is the Asset App",
+                            "1",
+                            "Not useful",
+                            "Usefull",
+                            "-",
+                            "-",
+                            "***replace chosenAnswer here***"
+                    };
+
+                    values[values.length-1] = ""+radioButton.getText();
+                    ArrayList<String> valuesList = new ArrayList<String>(Arrays.asList(values));
+                    surveyReading.setValues(valuesList);
+
+                        nn_VM.store(surveyReading);
+                        Toast.makeText(MainActivity.this,
+                                "Written to database", Toast.LENGTH_SHORT).show();
+
+
+
+
+                } else {
+                    Toast.makeText(MainActivity.this,
+                            "Nervousnet Service not running", Toast.LENGTH_SHORT).show();
+
+                }
+
+
             }
+
+        });
+
+        btnFetch.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Fetch reading from database.
+
+
+                try {
+                    nn_VM.getReading((long)10002, MainActivity.this);
+                } catch (NoSuchElementException e) {
+                    e.printStackTrace();
+                }
+
+
+
+
+
+            }
+
         });
 
 
-        batterySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                byte state = batterySwitch.isChecked() ? NervousnetVMConstants.SENSOR_STATE_AVAILABLE_DELAY_LOW : NervousnetVMConstants.SENSOR_STATE_AVAILABLE_BUT_OFF;
-                NNLog.d("MainActivity", "batterySwitch state = "+state);
 
-                EventBus.getDefault().post(new NNEvent((long) 1, state, NervousnetVMConstants.EVENT_CHANGE_SENSOR_STATE_REQUEST));
-            }
-        });
 
-        gyroSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                byte state = gyroSwitch.isChecked() ? NervousnetVMConstants.SENSOR_STATE_AVAILABLE_DELAY_LOW : NervousnetVMConstants.SENSOR_STATE_AVAILABLE_BUT_OFF;
-                NNLog.d("MainActivity", "gyroSwitch state = "+state);
+    }
 
-                EventBus.getDefault().post(new NNEvent((long) 2, state, NervousnetVMConstants.EVENT_CHANGE_SENSOR_STATE_REQUEST));
-            }
-        });
 
-        locationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                byte state = locationSwitch.isChecked() ? NervousnetVMConstants.SENSOR_STATE_AVAILABLE_DELAY_LOW : NervousnetVMConstants.SENSOR_STATE_AVAILABLE_BUT_OFF;
-                NNLog.d("MainActivity", "locationSwitch state = "+state);
+    @Override
+    public void success(List<SensorReading> list) throws RemoteException {
 
-                EventBus.getDefault().post(new NNEvent((long) 3, state, NervousnetVMConstants.EVENT_CHANGE_SENSOR_STATE_REQUEST));
-            }
-        });
+        SensorReading sensorReading = list.get(list.size()-1);
+        if(sensorReading != null) {
+            ArrayList<String> retreivedValues = sensorReading.getValues();
+
+            ansTV.setText("Survey Question: "+retreivedValues.get(1)+" \n Chosen Answer - "+retreivedValues.get(8));
+            Toast.makeText(MainActivity.this,retreivedValues.get(1)+" - "+retreivedValues.get(8), Toast.LENGTH_SHORT).show();
+
+        } else
+            Toast.makeText(MainActivity.this,"Survey database empty", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void failure(InfoReading infoReading) throws RemoteException {
+        Toast.makeText(MainActivity.this,"Error: "+infoReading.getInfoCode(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public IBinder asBinder() {
+        return null;
     }
 }
